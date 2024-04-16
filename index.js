682ae26ab82cb5ae64ac76e98b0d17edcd70c4f6
Derrick korir kiptoo <derrickroz4227@gmail.com>
2024-04-16 12:55:40 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(game => game.result === "W");
+    if (winningRecord) {
+      return winningRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+  
